8c1013d8e15930ed7cc0394bcf0162296d89c925
tharindu ariyarathna <tharinduariyarathna94@gmail.com>
2017-06-15 01:06:02 +0530

    Create Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,24 +1,25 @@
+//reverse values of integer..
 import java.util.*;
 public class Reverse{
   public static void main(String args[]){
     int rev;
 	
 	boolean n = true;
-	do{
-		try{
-	Scanner test1 =new Scanner(System.in);
-    System.out.println("Enter a Number ");
-    int num=test1.nextInt();
+	do{//if exception occure loop will run until get correct type of input
+		try{//to check user weather input is a integer or no
+	Scanner test1 =new Scanner(System.in); //scanner object
+    System.out.println("Enter a Number ");//call to user input
+    int num=test1.nextInt();//user input
 	
 	while(num!=0){
      
-     rev = num%10;
-     num = num/10;
-     System.out.print( rev );
+     rev = num%10;//last num of given input
+     num = num/10;//call to reamin 
+     System.out.print( rev );//last number list
      
     }
 	 System.out.print('\n');
-	n=true;
+	n=true;//goal is true
 	
 	
 	}catch(Exception e){
